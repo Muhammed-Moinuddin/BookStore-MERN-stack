57ee5dc7db35fae655cdf0fcab8296da3ddc1ea3
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2024-02-08 23:54:57 +0500

    Updated Header
    Worked on basic layout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
-import { AppBar, Box, Toolbar, Typography, IconButton } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, IconButton, ButtonGroup, Button} from '@mui/material';
+import {DeleteForever, Menu} from '@mui/icons-material';
+
+const navItems = ['Home', 'About', 'Contact'];
 
 const Header = () => {
   return (
-    <>
-      <Box sx={{ backgroundColor: '#262228', height: '10px' }}>
-        {/* Content or styling for the first Box */}
-      </Box>
-      <Box sx={{ display: 'flex', backgroundColor: 'blue' }}>
-        <AppBar position='fixed' sx={{ backgroundColor: '#262228' }}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              sx={{ mr: 2, display: { sm: 'none' } }}
-            ></IconButton>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-            >
-              MUI
-            </Typography>
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}></Box>
-          </Toolbar>
+    <Box sx={{ display: 'flex'}}>
+        <AppBar component="nav">
+            <Toolbar>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                    >
+                    <Menu />
+                </IconButton>
+                <Typography
+                    variant="h6"
+                    component="div"
+                >
+                    Book Store App
+                </Typography>
+                <Box sx={{ flexGrow: 1 }} />
+                <ButtonGroup variant="contained" aria-label="Basic button group">
+                    <Button>Table View</Button>
+                    <Button>Card View</Button>
+                </ButtonGroup> 
+                <Box sx={{ flexGrow: 1 }} />
+                <Button variant="contained" aria-label="Basic button group" startIcon={<DeleteForever />}>
+                    Delete
+                </Button> 
+            </Toolbar>
+
         </AppBar>
-      </Box>
-    </>
-  );
-};
+    </Box>
+  )
+}
 
 export default Header;
